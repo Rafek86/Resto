0a4b6477423dc0cde7acd7c5d2b63ff947297e5c
Rafek <rafeknaim@gamil.com>
2025-04-18 01:28:31 +0200

    AddOrder&TablesEnums

---
+++ src/Resto.Domain/Enums/TablesStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resto.Domain.Enums
+{
+  public  enum TablesStatus
+    {
+        Available,
+        Reserved
+    }
+}
