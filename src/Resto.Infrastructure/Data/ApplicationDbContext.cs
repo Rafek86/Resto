7ce40cb7713f3a54fd24f9108a96a67276c50148
Rafek <rafeknaim@gamil.com>
2025-04-18 01:29:56 +0200

    ApplicationDbContext

---
+++ src/Resto.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Resto.Application.Common.Interfaces;
+using Resto.Domain.Models;
+using Resto.Infrastructure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resto.Infrastructure.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : IdentityDbContext<ApplicationUser, IdentityRole, string>(options) ,IApplicationDbContext
+    {
+        
+
+        public DbSet<Customer> Customers { get; set; } 
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }  
+        public DbSet<Notification> Notifications { get; set; }      
+        public DbSet<Order> Orders { get; set; }    
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+
+}
