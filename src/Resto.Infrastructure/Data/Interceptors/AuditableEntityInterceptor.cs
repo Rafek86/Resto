d78407475023ad839b11cfba0d555eba0c70ecd6
Rafek <rafeknaim@gamil.com>
2025-04-18 01:29:32 +0200

    AddAuditableInterceptor

---
+++ src/Resto.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Resto.Domain.Common.Models;
+using System.Security.Principal;
+
+namespace Resto.Infrastructure.Data.Interceptors
+{
+    public class AuditableEntityInterceptor :SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateEntities(eventData.Context);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            UpdateEntities(eventData.Context);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        public void UpdateEntities(DbContext? dbContext) {
+
+            if (dbContext == null) return;
+
+            foreach (var entry in dbContext.ChangeTracker.Entries<AuditableEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedBy = "Rafek";
+                    entry.Entity.Created = DateTime.UtcNow;
+                }
+
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified || entry.HasChangedOwnedEntities())
+                {
+                    entry.Entity.LastModifiedBy = "Rafek";
+                    entry.Entity.LastModified = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static bool HasChangedOwnedEntities(this EntityEntry entry) =>
+            entry.References.Any(r =>
+                r.TargetEntry != null &&
+                r.TargetEntry.Metadata.IsOwned() &&
+                (r.TargetEntry.State == EntityState.Added || r.TargetEntry.State == EntityState.Modified));
+    }
+}
