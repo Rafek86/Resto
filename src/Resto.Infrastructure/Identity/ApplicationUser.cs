3a758480b76f06347e4d195afd1f1b26e9b0cace
Rafek <rafeknaim@gamil.com>
2025-04-18 01:30:17 +0200

    AddIdentity

---
+++ src/Resto.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Resto.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resto.Infrastructure.Identity
+{
+    public class ApplicationUser :IdentityUser
+    {
+        public Guid? CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+    }
+}
