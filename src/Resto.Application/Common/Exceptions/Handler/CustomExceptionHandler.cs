cb71770a7ca94f145bb94442425d7e736efe8fab
Rafek <rafeknaim@gamil.com>
2025-05-05 03:39:24 +0300

    Custom Exception

---
+++ src/Resto.Application/Common/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Resto.Application.Common.Exceptions.Handler
+{
+    public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError(
+                "Error Message : {exceptionMessage} , Time of occurrence {time}",
+                exception.Message, DateTime.UtcNow);
+
+            (string Details ,string Title,int StatusCode) details = exception switch
+            {
+                ValidationException => 
+                (
+                exception.Message,
+                exception.GetType().Name,
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+
+                NotFoundException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    httpContext.Response.StatusCode = StatusCodes.Status404NotFound
+                ),
+                UnauthorizedAccessException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized
+                ),
+                _ =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError
+                )
+            };
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = details.Title,
+                Status = details.StatusCode,
+                Detail = details.Details,
+                Instance = httpContext.Request.Path
+            };
+
+            problemDetails.Extensions.Add("traceId", httpContext.TraceIdentifier);
+
+            if (exception is ValidationException validationException)
+            {
+                problemDetails.Extensions.Add("errors", validationException.Errors);
+            }
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+        }
+    }
+}
